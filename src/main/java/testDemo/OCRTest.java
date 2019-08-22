60401e036521f99a958601f001de40f1d2d2bcb8
zhangxd <zhangxd@9186.com>
2019-08-22 10:56:16 +0800

    delete

--- src/main/java/testDemo/OCRTest.java
+++
@@ -1,8 +0,0 @@
-public class OCRTest {
-
-    public static void main(String args[]) {
-        String filePath = System.getProperty("user.dir");
-        String resText = OcrUtils.getStrFromPic(filePath + "//ocrTestPic1.png", "D:\\TessOCR\\Tesseract-OCR", "chi_sim+eng");
-        System.out.println("结果为："+resText);
-    }
-}
--- src/main/java/tools/SystemInfoUtils.java
+++
@@ -1,9 +0,0 @@
-import java.util.Properties;
-
-public class SystemInfoUtils {
-    static Properties props = System.getProperties();
-    public static String getSystemVersion() {
-        return props.getProperty("os.version");
-    }
-
-}
